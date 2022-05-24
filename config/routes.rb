8c3d7f2b66f7c8671f8cc63c15b35105249cc067
Florian Carriere <carriere.florian@gmail.com>
2022-05-24 11:45:33 +0200

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :user, only: %i[show edit update]
-  resources :teacher
-  resources :appointment, only: %i[show new create edit update]
+  resources :teacher do
+    resources :appointment, only: %i[show new create edit update]
+  end
 end
