5ffa18477e3b13c1fe0a2d500d0693236b2ac8a6
Jerome De Toffoli <j.detoffoli@orange.fr>
2022-05-24 14:14:58 +0200

    Create and new for teachers

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,10 @@
 class TeachersController < ApplicationController
+  def new
+    @teacher = Teacher.new
+  end
+
+  def create
+    @teacher = Teacher.new(teacher_params)
+    @teacher.save
+  end
 end
