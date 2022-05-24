52157214aeec414f07c1802982d083a4a693b682
Mathieu Dupin <dupin.mathieu@gmail.com>
2022-05-24 11:00:21 +0200

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "Creating users"
+
+User.create(username: "Jonathan84", email: "joz84@lewagon.com", password: "secret")
+User.create(username: "Patricia-MacLellan", email: "patricia@gmail.com", password: "secret")
+
+p "Creating teachers"
+
+Teacher.create(name: "Mélanie", location: "Bordeaux", language: "Rails, Ruby", price: "1000", user_id: "1")
+Teacher.create(name: "Paul", location: "Bordeaux", language: "Rails, Ruby, CSS", price: "100", user_id: "1")
+
+p "Finished !"
