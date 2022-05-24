32b3c2aa869f097cfc62677cfb01d8716ac3fdfe
Patricia Maclellan <patriciamaclellan848@gmail.com>
2022-05-24 12:46:01 +0200

    created user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    @users = User.find(params[:id])
+
+    respond_to do |appointment|
+      appointment.html#show
+    end
+  end
 end
