91ef9ae5755ec5fcc6b0b7d00fb227312218b443
Mathieu Dupin <dupin.mathieu@gmail.com>
2022-05-24 16:28:08 +0200

    fixed teacher controller

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,23 +1,24 @@
 class TeachersController < ApplicationController
- before_action :set_teacher
+  before_action :set_teacher, only: %i[show edit update destroy]
+
   def new
     @teacher = Teacher.new
   end
 
   def create
     @teacher = Teacher.new(teacher_params)
-    @teacher.save
-    redirect_to teacher_path(@teacher)
+
+    if @teacher.save
+      redirect_to teacher_path(@teacher)
+    else
+      render :new
+    end
   end
 
-  def teacher_params
-    params.require(:teacher).permit(:name, :location, :language, :availability, :price)
-  end
-  
   def index
     @teachers = Teacher.all
   end
-  
+
   def show
   end
 
@@ -28,5 +29,9 @@ class TeachersController < ApplicationController
 
   def set_teacher
     @teacher = Teacher.find(params[:id])
+  end
+
+  def teacher_params
+    params.require(:teacher).permit(:name, :location, :language, :availability, :price)
   end
 end
