5568d36eacda96f927a9d065e3ce9de4bf19d0d6
Mathieu Dupin <dupin.mathieu@gmail.com>
2022-05-25 16:16:06 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 p "Creating users"
 
 User.create(username: "Marceau", email: "marceau@lewagon.com", password: "secret")
-User.create(username: "Patricia-MacLellan", email: "patricia@gmail.com", password: "secret")
+User.create(username: "Emy", email: "emy@gmail.com", password: "secret")
 
 p "Creating teachers"
 
@@ -17,5 +17,6 @@ Teacher.create(name: "Kalid", location: "Bordeaux", language: "CSS, HTML, JS", p
 Teacher.create(name: "Jérome", location: "Bordeaux", language: "Rails, Ruby, JS", price: "100", user_id: "1")
 Teacher.create(name: "Florian", location: "Bordeaux", language: "Rails, CSS, JS", price: "100", user_id: "1")
 Teacher.create(name: "Mathieu", location: "Bordeaux", language: "React, React Native, Vue.JS", price: "5", user_id: "1")
+Teacher.create(name: "Patricia", location: "Bordeaux", language: "Python, SQL, C", price: "200", user_id: "1")
 
 p "Finished !"
